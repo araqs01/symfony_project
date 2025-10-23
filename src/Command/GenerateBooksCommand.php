6dc5616defb4da6c42bc799bfb8d7eed474041c8
araqs01 <araxiazurabyan001@gmail.com>
2025-10-23 22:50:58 +0400

    bug fix in command

--- src/Command/GenerateBooksCommand.php
+++ src/Command/GenerateBooksCommand.php
@@ -7,16 +7,19 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\DBAL\Connection;
 
 class GenerateBooksCommand extends Command
 {
     protected static $defaultName = 'app:generate-books';
     private $em;
+    private $conn;
     private $batchSize = 1000;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, Connection $conn)
     {
         $this->em = $em;
+        $this->conn = $conn;
         parent::__construct();
     }
 
@@ -31,34 +34,66 @@ class GenerateBooksCommand extends Command
     {
         $authorsData = ['Author One', 'Author Two', 'Author Three'];
 
+        $output->writeln("Cleaning old data...");
+        $this->conn->executeStatement('DELETE FROM books');
+        $this->conn->executeStatement('DELETE FROM authors');
+        $output->writeln("Old data removed.");
+
         foreach ($authorsData as $authorName) {
-            $author = new Author($authorName);
-            $this->em->persist($author);
-            $this->em->flush();
-            $this->em->clear();
+            // Avoid duplicate author
+            $existingAuthor = $this->em->getRepository(Author::class)->findOneBy(['name' => $authorName]);
+            if (!$existingAuthor) {
+                $author = new Author($authorName);
+                $this->em->persist($author);
+                $this->em->flush();
+            } else {
+                $author = $existingAuthor;
+            }
 
-            $author = $this->em->getRepository(Author::class)->findOneBy(['name'=>$authorName]);
+            $authorId = $author->getId();
 
-            $i = 0;
-            foreach (range(1, 100000) as $num) {
-                $book = new Book("Book $num by " . $author->getName(), $author);
-                $this->em->persist($book);
-                $i++;
+            $output->writeln("Generating books for $authorName...");
 
-                if ($i % $this->batchSize === 0) {
-                    $this->em->flush();
-                    $this->em->clear();
-                    $author = $this->em->getRepository(Author::class)->findOneBy(['name'=>$authorName]);
-                    $output->writeln("Inserted $i books for $authorName...");
+            $booksData = [];
+            for ($num = 1; $num <= 100000; $num++) {
+                $booksData[] = [
+                    'title' => "Book $num by " . $author->getName(),
+                    'author_id' => $authorId
+                ];
+
+                if (count($booksData) >= $this->batchSize) {
+                    $this->insertBooks($booksData);
+                    $booksData = [];
+                    $output->writeln("Inserted $num books for $authorName...");
                 }
             }
 
-            $this->em->flush();
-            $this->em->clear();
+            if (count($booksData) > 0) {
+                $this->insertBooks($booksData);
+            }
+
             $output->writeln("Finished author: $authorName");
         }
 
         $output->writeln('Books generated successfully!');
         return Command::SUCCESS;
     }
+
+    private function insertBooks(array $booksData): void
+    {
+        $values = [];
+        $params = [];
+        $i = 0;
+
+        foreach ($booksData as $book) {
+            $values[] = "(:title$i, :author_id$i)";
+            $params["title$i"] = $book['title'];
+            $params["author_id$i"] = $book['author_id'];
+            $i++;
+        }
+
+        $sql = 'INSERT INTO books (title, author_id) VALUES ' . implode(', ', $values);
+        $this->conn->executeStatement($sql, $params);
+    }
+
 }
